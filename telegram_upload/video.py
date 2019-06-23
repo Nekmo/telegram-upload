71761386ae8e2d45c7fb38cecb35eb802f25ebce
Nekmo <contacto@nekmo.com>
2019-06-23 14:53:14 +0200

    Issue #21: Windows support for videos

--- telegram_upload/video.py
+++ telegram_upload/video.py
@@ -1,8 +1,10 @@
+import platform
 import struct
 import subprocess
 import tempfile
 import os
 
+import click
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
@@ -23,19 +25,28 @@ def video_metadata(file):
     return extractMetadata(createParser(file))
 
 
+def get_ffmpeg_command():
+    return os.environ.get('FFMPEG_COMMAND',
+                          'ffmpeg.exe' if platform.system() == 'Windows' else 'ffmpeg')
+
+
 def get_video_thumb(file, output=None, width=90):
     output = output or tempfile.NamedTemporaryFile(suffix='.jpg').name
     metadata = video_metadata(file)
     duration = metadata.get('duration').seconds if metadata.has('duration') else 0
-    p = subprocess.Popen([
-        'ffmpeg',
-        '-ss', str(int(duration / 2)),
-        '-i', file,
-        '-filter:v',
-        'scale={}:-1'.format(width),
-        '-vframes:v', '1',
-        output,
-    ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    try:
+        p = subprocess.Popen([
+            get_ffmpeg_command(),
+            '-ss', str(int(duration / 2)),
+            '-i', file,
+            '-filter:v',
+            'scale={}:-1'.format(width),
+            '-vframes:v', '1',
+            output,
+        ], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    except FileNotFoundError:
+        click.echo('ffmpeg command is not available. Thumbnails for videos are not available!', err=True)
+        return None
     p.communicate()
     if not p.returncode and os.path.lexists(file):
         return output
