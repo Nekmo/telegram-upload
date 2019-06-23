45867ada53ff719ae01262d05ba2e9ee6c6f7d06
Nekmo <contacto@nekmo.com>
2019-06-23 16:00:39 +0200

    Issue #23: Create ~/.config directory if not exists

--- telegram_upload/config.py
+++ telegram_upload/config.py
@@ -9,6 +9,7 @@ CONFIG_FILE = os.path.expanduser('~/.config/telegram-upload.json')
 def default_config():
     if os.path.lexists(CONFIG_FILE):
         return CONFIG_FILE
+    os.makedirs(os.path.dirname(CONFIG_FILE), exist_ok=True)
     click.echo('Go to https://my.telegram.org and create a App in API development tools')
     api_id = click.prompt('Please Enter api_id', type=int)
     api_hash = click.prompt('Now enter api_hash')
