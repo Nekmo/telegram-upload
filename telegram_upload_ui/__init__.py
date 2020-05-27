4231eb4329c9d29fc27d759039bdc4f9a7f13a2e
Nekmo <contacto@nekmo.com>
2020-05-27 02:06:45 +0200

    Issue #41: Upload user interface (table).

--- telegram_upload_ui/__init__.py
+++ telegram_upload_ui/__init__.py
@@ -1,6 +1,8 @@
 import sys
 from PySide2 import QtWidgets
-from PySide2.QtGui import QIcon
+from PySide2.QtCore import Qt
+from PySide2.QtGui import QIcon, QBrush, QPen
+from PySide2.QtWidgets import QStyle
 
 
 class Form(QtWidgets.QMainWindow):
@@ -8,8 +10,8 @@ class Form(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
-        textEdit = QtWidgets.QTextEdit()
-        self.setCentralWidget(textEdit)
+        # textEdit = QtWidgets.QTextEdit()
+        # self.setCentralWidget(textEdit)
 
         # https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
         exitAct = QtWidgets.QAction(QIcon.fromTheme("application-exit"), 'Exit', self)
@@ -33,6 +35,12 @@ class Form(QtWidgets.QMainWindow):
         toolbar.addAction(selectFiles)
         toolbar.addAction(exitAct)
 
+        self.createTable()
+        self.setCentralWidget(self.tableWidget)
+        # self.layout = QtWidgets.QVBoxLayout()
+        # self.layout.addWidget(self.tableWidget)
+        # self.setLayout(self.layout)
+
         self.setGeometry(300, 300, 350, 250)
         self.setWindowTitle('Main window')
         self.show()
@@ -45,6 +53,17 @@ class Form(QtWidgets.QMainWindow):
         if dlg.exec_():
             filenames = dlg.selectedFiles()
             print(filenames)
+
+    def createTable(self):
+        # Create table
+        self.tableWidget = QtWidgets.QTableWidget(1, 3)
+        item1 = QtWidgets.QTableWidgetItem("foo")
+        comboBox = QtWidgets.QComboBox()
+        progressBar = QtWidgets.QProgressBar()
+        progressBar.setValue(50)
+        self.tableWidget.setItem(0, 0, item1)
+        self.tableWidget.setCellWidget(0, 1, comboBox)
+        self.tableWidget.setCellWidget(0, 2, progressBar)
 
 
 if __name__ == '__main__':
