6d34c530bc7bc7b87ea5d0d5f0370eeb2fab9b2e
Nekmo <contacto@nekmo.com>
2020-11-15 02:04:02 +0100

    Issue #43: Write tests (TestParseProxyString).

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,10 +3,11 @@ import os
 import unittest
 from unittest.mock import patch, mock_open, sentinel, Mock
 
+import socks
 from telethon.tl.types import DocumentAttributeFilename
 
-from telegram_upload.client import Client
-from telegram_upload.exceptions import TelegramUploadDataLoss, TelegramUploadNoSpaceError
+from telegram_upload.client import Client, parse_proxy_string
+from telegram_upload.exceptions import TelegramUploadDataLoss, TelegramUploadNoSpaceError, TelegramProxyError
 
 CONFIG_DATA = {'api_hash': '', 'api_id': ''}
 
@@ -17,6 +18,34 @@ class AnyArg(object):
     """https://stackoverflow.com/questions/20428750/pythons-assert-called-with-is-there-a-wildcard-character"""
     def __eq__(a, b):
         return True
+
+
+class TestParseProxyString(unittest.TestCase):
+    def test_none(self):
+        self.assertIsNone(parse_proxy_string(None))
+
+    def test_malformed_url(self):
+        with self.assertRaises(TelegramProxyError):
+            parse_proxy_string('foo')
+
+    def test_mtproxy(self):
+        s = parse_proxy_string('mtproxy://secret@foo:123')
+        self.assertEqual(s, ('mtproxy', 'foo', 123, 'secret'))
+
+    @patch('builtins.__import__', side_effect=ImportError)
+    def test_socks_import_error(self, m):
+        with self.assertRaises(TelegramProxyError):
+            parse_proxy_string('socks4://user:pass@foo:123')
+
+    def test_unsupported_proxy_type(self):
+        with self.assertRaises(TelegramProxyError):
+            parse_proxy_string('foo://user:pass@foo:123')
+
+    def test_proxy(self):
+        self.assertEqual(
+            parse_proxy_string('http://user:pass@foo:123'),
+            (socks.HTTP, 'foo', 123, True, 'user', 'pass')
+        )
 
 
 class TestClient(unittest.TestCase):
