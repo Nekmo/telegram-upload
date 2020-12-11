0b8d49d61cb34c6f2e3c4ba12bc648be681334b5
Nekmo <contacto@nekmo.com>
2020-12-12 00:23:09 +0100

    Issue #43: Write tests (TestScanTree).

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest.mock import patch, Mock
 
-from telegram_upload.utils import sizeof_fmt
+from telegram_upload.utils import sizeof_fmt, scantree
 
 
 class TestSizeOfFmt(unittest.TestCase):
@@ -12,3 +13,26 @@ class TestSizeOfFmt(unittest.TestCase):
 
     def test_exact_mebibytes(self):
         self.assertEqual(sizeof_fmt((1024 ** 2) * 3), '3.0MiB')
+
+
+class TestScanTree(unittest.TestCase):
+    @patch('telegram_upload.utils.scandir', return_value=[])
+    def test_empty_directory(self, m):
+        self.assertEqual(list(scantree('foo')), [])
+
+    @patch('telegram_upload.utils.scandir')
+    def test_files(self, m):
+        file = Mock()
+        file.is_dir.return_value = False
+        m.return_value = [file] * 3
+        self.assertEqual(list(scantree('foo')), m.return_value)
+
+    @patch('telegram_upload.utils.scandir')
+    def test_directory(self, m):
+        directory = Mock()
+        directory.is_dir.side_effect = [True, False]
+        file = Mock()
+        file.is_dir.return_value = False
+        side_effect = [[directory], [file] * 3]
+        m.side_effect = side_effect
+        self.assertEqual(list(scantree('foo')), side_effect[-1])
