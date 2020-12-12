9332ed6fe6597f3d7b5935e79c4405a114458c1d
Nekmo <contacto@nekmo.com>
2020-12-12 23:43:19 +0100

    Issue #43: Write tests (TestDefaultConfig).

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,3 +9,13 @@ class TestDefaultConfig(unittest.TestCase):
     def test_exists(self, m):
         self.assertEqual(default_config(), CONFIG_FILE)
         m.assert_called_once()
+
+    @patch('telegram_upload.config.os')
+    @patch('telegram_upload.config.click')
+    @patch('telegram_upload.config.json')
+    def test_create(self, m_json, m_click, m_os):
+        m_os.path.lexists.return_value = False
+        m_click.prompt.side_effect = ['api_id', 'api_hash']
+        self.assertEqual(default_config(), CONFIG_FILE)
+        m_json.dump.assert_called_once()
+        self.assertEqual(m_json.dump.call_args[0][0], {'api_id': 'api_id', 'api_hash': 'api_hash'})
