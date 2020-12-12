130420474b39b4711920b72fd76fc9fe3fac1611
Nekmo <contacto@nekmo.com>
2020-12-12 23:47:56 +0100

    Issue #43: Write tests (TestDefaultConfig).

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ class TestDefaultConfig(unittest.TestCase):
     @patch('telegram_upload.config.os.path.lexists', return_value=True)
     def test_exists(self, m):
         self.assertEqual(default_config(), CONFIG_FILE)
-        m.assert_called_once()
+        self.assertEqual(m.call_count, 1)
 
     @patch('telegram_upload.config.os')
     @patch('telegram_upload.config.click')
@@ -17,5 +17,5 @@ class TestDefaultConfig(unittest.TestCase):
         m_os.path.lexists.return_value = False
         m_click.prompt.side_effect = ['api_id', 'api_hash']
         self.assertEqual(default_config(), CONFIG_FILE)
-        m_json.dump.assert_called_once()
+        self.assertEqual(m_json.dump.call_count, 1)
         self.assertEqual(m_json.dump.call_args[0][0], {'api_id': 'api_id', 'api_hash': 'api_hash'})
