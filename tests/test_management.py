c18aacb0852ea7d937c831dc7e4b32b855e1c3f6
Nekmo <contacto@nekmo.com>
2020-12-30 23:54:40 +0100

    Issue #43: Write tests (TestUpload).

---
+++ tests/test_management.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+from unittest.mock import patch
+
+from click.testing import CliRunner
+
+from telegram_upload.management import upload
+
+
+directory = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestUpload(unittest.TestCase):
+    @patch('telegram_upload.management.Client')
+    def test_upload(self, m):
+        test_file = os.path.join(directory, 'test_management.py')
+        runner = CliRunner()
+        result = runner.invoke(upload, [test_file])
+        self.assertEqual(result.exit_code, 0)
+        m.assert_called_once()
+        m.return_value.send_files.assert_called_once()
+
+    @patch('telegram_upload.management.Client')
+    def test_failed(self, m):
+        runner = CliRunner()
+        result = runner.invoke(upload, ['missing_file.txt'])
+        self.assertEqual(result.exit_code, 1)
+        m.return_value.send_files.assert_not_called()
