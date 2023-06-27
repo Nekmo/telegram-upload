430ded28df88e41a471f76465e81b18760e88452
Nekmo <contacto@nekmo.com>
2023-06-28 01:59:36 +0200

    Issue #159: Telegram premium (tests).

--- tests/test_client/test_telegram_manager_client.py
+++ tests/test_client/test_telegram_manager_client.py
@@ -6,7 +6,8 @@ import socks
 from telethon.network import ConnectionTcpMTProxyRandomizedIntermediate
 
 from telegram_upload.client import TelegramManagerClient
-from telegram_upload.client.telegram_manager_client import phone_match, parse_proxy_string
+from telegram_upload.client.telegram_manager_client import phone_match, parse_proxy_string, USER_MAX_FILE_SIZE, \
+    BOT_USER_MAX_FILE_SIZE, PREMIUM_USER_MAX_FILE_SIZE, USER_MAX_CAPTION_LENGTH, PREMIUM_USER_MAX_CAPTION_LENGTH
 from telegram_upload.config import SESSION_FILE
 from telegram_upload.exceptions import TelegramProxyError
 
@@ -84,3 +85,39 @@ class TestTelegramManagerClient(unittest.TestCase):
             phone=phone, password=password, bot_token=bot_token, force_sms=force_sms, first_name=first_name,
             last_name=last_name, max_attempts=max_attempts,
         )
+
+    @patch('builtins.open', mock_open(read_data=json.dumps(CONFIG_DATA)))
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.__init__', return_value=None)
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.get_me')
+    def test_me(self, mock_get_me: MagicMock, _: MagicMock):
+        me_result = TelegramManagerClient(MagicMock()).me
+        mock_get_me.assert_called_once_with()
+        self.assertEqual(mock_get_me.return_value, me_result)
+
+    @patch('builtins.open', mock_open(read_data=json.dumps(CONFIG_DATA)))
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.__init__', return_value=None)
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.me')
+    def test_max_file_size(self, mock_me: MagicMock, _: MagicMock):
+        with self.subTest("Test user max file size"):
+            mock_me.premium = False
+            mock_me.bot = False
+            self.assertEqual(TelegramManagerClient(MagicMock()).max_file_size, USER_MAX_FILE_SIZE)
+        with self.subTest("Test bot max file size"):
+            mock_me.premium = False
+            mock_me.bot = True
+            self.assertEqual(TelegramManagerClient(MagicMock()).max_file_size, BOT_USER_MAX_FILE_SIZE)
+        with self.subTest("Test premium user max file size"):
+            mock_me.premium = True
+            mock_me.bot = False
+            self.assertEqual(TelegramManagerClient(MagicMock()).max_file_size, PREMIUM_USER_MAX_FILE_SIZE)
+
+    @patch('builtins.open', mock_open(read_data=json.dumps(CONFIG_DATA)))
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.__init__', return_value=None)
+    @patch('telegram_upload.client.telegram_manager_client.TelegramManagerClient.me')
+    def test_max_caption_length(self, mock_me: MagicMock, _: MagicMock):
+        with self.subTest("Test user max caption length"):
+            mock_me.premium = False
+            self.assertEqual(TelegramManagerClient(MagicMock()).max_caption_length, USER_MAX_CAPTION_LENGTH)
+        with self.subTest("Test premium user max caption length"):
+            mock_me.premium = True
+            self.assertEqual(TelegramManagerClient(MagicMock()).max_caption_length, PREMIUM_USER_MAX_CAPTION_LENGTH)
