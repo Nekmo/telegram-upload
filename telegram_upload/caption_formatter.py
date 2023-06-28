10e87c8fd5afd6608b2129cc37ddf730ed0c2288
Nekmo <contacto@nekmo.com>
2023-06-28 07:25:21 +0200

    Issue #115: Feature request - Mimic the folder structure when uploading files

--- telegram_upload/caption_formatter.py
+++ telegram_upload/caption_formatter.py
@@ -1,12 +1,15 @@
 import _string
 import datetime
 import hashlib
+import mimetypes
 import os
+import sys
 import zlib
 from pathlib import Path, PosixPath, WindowsPath
 from string import Formatter
 from typing import Any, Sequence, Mapping, Tuple
 
+from telegram_upload.video import video_metadata
 
 try:
     from typing import LiteralString
@@ -14,12 +17,165 @@ except ImportError:
     LiteralString = str
 
 
+if sys.version_info < (3, 8):
+    cached_property = property
+else:
+    from functools import cached_property
+
+
 CHUNK_SIZE = 4096
 VALID_TYPES: Tuple[Any, ...] = (str, int, float, complex, bool, datetime.datetime, datetime.date, datetime.time)
+AUTHORIZED_METHODS = (Path.absolute, Path.home)
+AUTHORIZED_STRING_METHODS = ("title", "capitalize", "lower", "upper", "swapcase", "strip", "lstrip", "rstrip")
+AUTHORIZED_DT_METHODS = (
+    "astimezone", "ctime", "date", "dst", "isoformat", "isocalendar", "isoformat", "isoweekday", "now", "time",
+    "timestamp", "timetuple", "timetz", "today", "toordinal", "tzname", "utcnow", "utcoffset", "utctimetuple", "weekday"
+)
+
+
+class Duration:
+    def __init__(self, seconds: int):
+        self.seconds = seconds
+
+    @property
+    def as_minutes(self):
+        return self.seconds // 60
+
+    @property
+    def as_hours(self):
+        return self.as_minutes // 60
+
+    @property
+    def as_days(self):
+        return self.as_hours // 24
+
+    @property
+    def for_humans(self):
+        words = ["year", "day", "hour", "minute", "second"]
+
+        if not self.seconds:
+            return "now"
+        else:
+            m, s = divmod(self.seconds, 60)
+            h, m = divmod(m, 60)
+            d, h = divmod(h, 24)
+            y, d = divmod(d, 365)
+
+            time = [y, d, h, m, s]
+
+            duration = []
+
+            for x, i in enumerate(time):
+                if i == 1:
+                    duration.append(f"{i} {words[x]}")
+                elif i > 1:
+                    duration.append(f"{i} {words[x]}s")
+
+            if len(duration) == 1:
+                return duration[0]
+            elif len(duration) == 2:
+                return f"{duration[0]} and {duration[1]}"
+            else:
+                return ", ".join(duration[:-1]) + " and " + duration[-1]
+
+    def __int__(self):
+        return self.seconds
+
+    def __str__(self):
+        return str(self.seconds)
+
+
+class FileSize:
+    def __init__(self, size: int):
+        self.size = size
+
+    @property
+    def as_kilobytes(self):
+        return self.size // 1000
+
+    @property
+    def as_megabytes(self):
+        return self.as_kilobytes // 1000
+
+    @property
+    def as_gigabytes(self):
+        return self.as_megabytes // 1000
+
+    @property
+    def as_kibibytes(self):
+        return self.size // 1024
+
+    @property
+    def as_mebibytes(self):
+        return self.as_kibibytes // 1024
+
+    @property
+    def as_gibibytes(self):
+        return self.as_mebibytes // 1024
+
+    @property
+    def for_humans(self, suffix="B"):
+        num = self.size
+        for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+            if abs(num) < 1024.0:
+                return f"{num:3.1f} {unit}{suffix}"
+            num /= 1024.0
+        return f"{num:.1f} Yi{suffix}"
+
+    def __int__(self):
+        return self.size
+
+    def __str__(self):
+        return str(self.size)
+
+
+class FileMedia:
+    def __init__(self, path: str):
+        self.path = path
+        self.metadata = video_metadata(path)
+
+    @cached_property
+    def video_metadata(self):
+        metadata = self.metadata
+        meta_groups = None
+        if hasattr(metadata, '_MultipleMetadata__groups'):
+            # Is mkv
+            meta_groups = metadata._MultipleMetadata__groups
+        if metadata is not None and not metadata.has('width') and meta_groups:
+            return meta_groups[next(filter(lambda x: x.startswith('video'), meta_groups._key_list))]
+        return metadata
+
+    @property
+    def duration(self):
+        if self.metadata and self.metadata.has('duration'):
+            return Duration(self.metadata.get('duration').seconds)
+
+    @property
+    def width(self):
+        return self.video_metadata.get('width') if self.video_metadata and self.video_metadata.has('width') else None
+
+    @property
+    def height(self):
+        return self.video_metadata.get('height') if self.video_metadata and self.video_metadata.has('height') else None
+
+    @property
+    def title(self):
+        return self.metadata.get('title') if self.metadata and self.metadata.has('title') else None
+
+    @property
+    def artist(self):
+        return self.metadata.get('artist') if self.metadata and self.metadata.has('artist') else None
+
+    @property
+    def album(self):
+        return self.metadata.get('album') if self.metadata and self.metadata.has('album') else None
+
+    @property
+    def producer(self):
+        return self.metadata.get('producer') if self.metadata and self.metadata.has('producer') else None
 
 
 class FileMixin:
-    _private = 4
 
     def _calculate_hash(self, hash_calculator: Any):
         with open(str(self), "rb") as f:
@@ -88,6 +244,35 @@ class FileMixin:
                     calculated_hash += 2 ** 32
             return hex(calculated_hash)[2:10].zfill(8)
 
+    @cached_property
+    def _file_stat(self):
+        return os.stat(str(self))
+
+    @cached_property
+    def ctime(self):
+        return datetime.datetime.fromtimestamp(self._file_stat.st_ctime)
+
+    @cached_property
+    def mtime(self):
+        return datetime.datetime.fromtimestamp(self._file_stat.st_mtime)
+
+    @cached_property
+    def atime(self):
+        return datetime.datetime.fromtimestamp(self._file_stat.st_atime)
+
+    @cached_property
+    def size(self):
+        return FileSize(self._file_stat.st_size)
+
+    @cached_property
+    def media(self):
+        return FileMedia(str(self))
+
+    @cached_property
+    def mimetype(self):
+        mimetypes.init()
+        return mimetypes.guess_type(str(self))[0]
+
 
 class FilePath(FileMixin, Path):
     def __new__(cls, *args, **kwargs):
@@ -115,6 +300,13 @@ class CaptionFormatter(Formatter):
             if "._" in field_name:
                 raise TypeError(f'Access to private property in {field_name}')
             obj, first = super().get_field(field_name, args, kwargs)
+            has_func = hasattr(obj, "__func__")
+            has_self = hasattr(obj, "__self__")
+            if (has_func and obj.__func__ in AUTHORIZED_METHODS) or \
+                    (has_self and isinstance(obj.__self__, str) and obj.__name__ in AUTHORIZED_STRING_METHODS) or \
+                    (has_self and isinstance(obj.__self__, datetime.datetime)
+                     and obj.__name__ in AUTHORIZED_DT_METHODS):
+                obj = obj()
             if not isinstance(obj, VALID_TYPES + (WindowsFilePath, PosixFilePath, FilePath)):
                 raise TypeError(f'Invalid type for {field_name}: {type(obj)}')
             return obj, first
