b31c7282bda8755f835756fe94db767c6ecfdb8c
Nekmo <contacto@nekmo.com>
2023-07-05 04:38:04 +0200

    Issue #202: Upload benchmarks

--- docs/upload_benchmark.py
+++ docs/upload_benchmark.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 import asyncio
+import json
 import os
 import time
+from itertools import groupby
+from statistics import mean, median
 from tempfile import NamedTemporaryFile
-from typing import Callable, Optional
+from typing import Callable, Optional, TypedDict, List, cast
 
 import click
 from telethon.tl.patched import Message
+import matplotlib.pyplot as plt
 
+from telegram_upload.caption_formatter import FileSize
 from telegram_upload.client import TelegramManagerClient
 from telegram_upload.config import default_config
 from telegram_upload.upload_files import NoLargeFiles
@@ -16,11 +21,29 @@ CHUNK = 1024 * 4
 REPEATS = 5
 BENCHMARKS = {
     "small": (1024 * 512, 3, 10),
-    # "medium": (1024 * 1024 * 20, 10, 10),
+    "medium": (1024 * 1024 * 20, 10, 10),
     # "large": (1024 * 1024 * 200, 30, 5),
     # "full": (1024 * 1024 * 1024 * 2, 90, 5),
 }
 PARALLELS = range(1, 11)
+DEFAULT_PARALLEL = 4
+RESULTS_FILE = 'upload_benchmark.json'
+
+
+class BenchmarkResultBreakdown(TypedDict):
+    """Benchmark result breakdown dict"""
+    minimum: float
+    maximum: float
+    average: float
+    median: float
+    times: List[float]
+
+
+class BenchmarkResult(TypedDict):
+    """Benchmark result dict"""
+    size: int
+    parallel: int
+    benchmark: BenchmarkResultBreakdown
 
 
 def create_file(size: int) -> str:
@@ -64,7 +87,14 @@ class Benchmark:
         """Return the average time"""
         if not self.times:
             return .0
-        return sum(self.times) / len(self.times)
+        return mean(self.times)
+
+    @property
+    def median(self) -> float:
+        """Return the median time"""
+        if not self.times:
+            return .0
+        return median(self.times)
 
     @property
     def minimum(self) -> float:
@@ -78,9 +108,10 @@ class Benchmark:
 
 
 def benchmark_file_size(client: TelegramManagerClient, size: int, repeats: int = REPEATS, wait: int = 0,
-                        parallel: Optional[int] = None):
+                        parallel: Optional[int] = None) -> BenchmarkResult:
     """Benchmark the upload of a file of the specified size"""
     # reset parallel upload blocks
+    parallel = cast(int, parallel or DEFAULT_PARALLEL)
     client.parallel_upload_blocks = parallel
     client.reconnecting_lock = asyncio.Lock()
     client.upload_semaphore = asyncio.Semaphore(parallel)
@@ -90,6 +121,7 @@ def benchmark_file_size(client: TelegramManagerClient, size: int, repeats: int =
     benchmark = Benchmark(lambda: upload_file(client, path), repeats, wait)
     benchmark()
     click.echo(f"Size: {size} bytes  -  Parallel: {parallel}")
+    click.echo(f"Median: {benchmark.median} seconds")
     click.echo(f"Average: {benchmark.average} seconds")
     click.echo(f"Minimum: {benchmark.minimum} seconds")
     click.echo(f"Maximum: {benchmark.maximum} seconds")
@@ -97,13 +129,31 @@ def benchmark_file_size(client: TelegramManagerClient, size: int, repeats: int =
     os.remove(path)
     for message in benchmark.results:
         message.delete()
+    return {
+        "size": size,
+        "parallel": parallel,
+        "benchmark": {
+            "minimum": benchmark.minimum,
+            "maximum": benchmark.maximum,
+            "average": benchmark.average,
+            "median": benchmark.median,
+            "times": benchmark.times,
+        }
+    }
 
 
-@click.command()
+@click.group()
+def cli():
+    """Console script for requirements-rating."""
+    pass
+
+
+@cli.command()
 @click.option('--repeats', '-r', default=None, type=int, help='Number of repeats')
 @click.option('--benchmark', '-b', default=None, type=click.Choice(list(BENCHMARKS.keys())), help='Benchmark name')
 @click.option('--parallel', '-p', default=None, type=int, help='Parallel parts uploaded')
-def benchmark(repeats, benchmark, parallel):
+@click.option('--results-file', '-f', default=RESULTS_FILE, type=str, help='JSON results file')
+def benchmark(repeats, benchmark, parallel, results_file):
     client = TelegramManagerClient(default_config())
     client.start()
     if benchmark:
@@ -114,10 +164,41 @@ def benchmark(repeats, benchmark, parallel):
         parallels = [parallel]
     else:
         parallels = PARALLELS
+    results = []
     for size, wait, def_repeats in benchmarks:
         for parallel in parallels:
-            benchmark_file_size(client, size, repeats or def_repeats, wait, parallel)
+            benchmark_result = benchmark_file_size(client, size, repeats or def_repeats, wait, parallel)
+            results.append(benchmark_result)
+    with open(results_file, 'w') as file:
+        json.dump(results, file, indent=4)
+
+
+@cli.command()
+@click.option('--results-file', '-f', default=RESULTS_FILE, type=click.Path(exists=True, dir_okay=False),
+              help='JSON results file')
+def graphs(results_file):
+    with open(results_file, 'r') as file:
+        results: List[BenchmarkResult] = json.load(file)
+    results_grouped = groupby(results, lambda x: x["size"])
+    for key, grouped in results_grouped:
+        grouped = list(grouped)
+        fig, ax = plt.subplots()
+        filesize = FileSize(key)
+        ax.errorbar(
+            [x["parallel"] for x in grouped],
+            [x["benchmark"]["median"] for x in grouped],
+            capsize=4,
+            yerr=[[x["benchmark"]["median"] - x["benchmark"]["minimum"] for x in grouped],
+                  [x["benchmark"]["maximum"] - x["benchmark"]["median"] for x in grouped]],
+            marker='.',
+            label=filesize.for_humans)
+        plt.legend()
+        plt.xlabel("Parallel parts")
+        plt.ylabel("Time (seconds)")
+        plt.grid()
+        plt.title(f"Upload time for {filesize.for_humans} file (less time is better)")
+        plt.savefig(f"benchmark_{filesize.for_humans.replace(' ', '_')}.png", dpi=150)
 
 
 if __name__ == '__main__':
-    benchmark()
+    cli()
